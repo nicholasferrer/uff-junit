300799f98b25abcb7948dfea5bead7bfc3de4584
Nicholas Ferrer <nicholasferrer@hotmail.com>
2021-02-15 16:52:42 -0300

    Adiciona casos de testes em calculadoraTest

--- test/calculadora/CalculadoraTest.java
+++ test/calculadora/CalculadoraTest.java
@@ -1,6 +1,3 @@
-package calculadora;
-
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -10,6 +7,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import calculadora.Calculadora;
 
 @DisplayName("Classe para teste da calculadora")
 public class CalculadoraTest {
@@ -21,33 +20,55 @@ public class CalculadoraTest {
 		calc = new Calculadora();
 	}
 	
-	@DisplayName("Testa a soma de dois n�meros")
+	@DisplayName("Testa a soma de dois números")
 	@Test
 	public void testSomaDoisNumeros() {
 		int soma = calc.soma(4, 5);		
 		Assertions.assertEquals(9, soma);		
 	}
 	
+	@DisplayName("Testa a divisão de dois números")
 	@Test
 	public void testDivisaoDoisNumeros() {
 		int divisao = calc.divisao(8, 4);
 		assertTrue(divisao == 2);
 	}
 	
+	@DisplayName("Testa a divisão por zero")
 	@Test
 	public void testDivisaoPorZero() {
 		try {
 			int divisao = calc.divisao(8, 0);
-			fail("Exce��o n�o lan�ada");
+			fail("Exceção não lançada");
 		}catch (ArithmeticException e) {
 			assertEquals("/ by zero", e.getMessage());
 		}		
 	}
 	
+	@DisplayName("Testa a divisão por zero com exception")
 	@Test
 	public void testDivisaoPorZeroComAssertThrows() {
 		assertThrows(ArithmeticException.class,
 				() -> calc.divisao(8, 0));
+	}
+
+	@DisplayName("Testa a somatória")
+	@Test
+	public void testSomatoria() {
+		int somatoria = calc.somatoria(4);		
+		Assertions.assertEquals(10, somatoria);		
+	}
+
+	@DisplayName("Testa se um número é positivo")
+	@Test
+	public void testNumeroEhPositivo() {	
+		Assertions.assertTrue(calc.ehPositivo(5));		
+	}
+
+	@DisplayName("Testa a comparação entre dois números")
+	@Test
+	public void testComparaDoisNumeros() {
+		Assertions.assertEquals(0, calc.compara(5, 5));		
 	}
 
 }
